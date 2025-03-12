e21c882dd2eb7a14549a5250975d61896853c00f
Dulip Sameera <dsgitrepo101@gmail.com>
2025-03-12 14:02:35 +0530

    handle connection release

--- src/main/java/lk/ijse/dep13/wenasconnectionpool/ReleaseConnectionServlet.java
+++ src/main/java/lk/ijse/dep13/wenasconnectionpool/ReleaseConnectionServlet.java
@@ -5,8 +5,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.dep13.wenasconnectionpool.db.ConnectionPool;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -36,11 +38,21 @@ public class ReleaseConnectionServlet extends HttpServlet {
         releaseConnection(id.trim(), resp);
     }
 
-    private void releaseConnection(String id, HttpServletResponse res) {
-        System.out.println("ReleaseConnectionServlet");
+    private void releaseConnection(String id, HttpServletResponse resp) throws IOException {
+        ConnectionPool conPool = (ConnectionPool) getServletContext().getAttribute("datasource");
+        conPool.releaseConnection(UUID.fromString(id));
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Connection ID: " + id + " released</h1>");
     }
 
-    private void releaseAllConnections(HttpServletResponse resp) {
-        System.out.println("ReleaseAllConnectionServlet");
+    private void releaseAllConnections(HttpServletResponse resp) throws IOException {
+        ConnectionPool conPool = (ConnectionPool) getServletContext().getAttribute("datasource");
+        conPool.releaseAllConnections();
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>All Connections released</h1>");
     }
 }
