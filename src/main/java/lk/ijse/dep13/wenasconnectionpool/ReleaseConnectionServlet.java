13dd2469e4f506183d392ad16e8f7b467a4df4ff
Dulip Sameera <dsgitrepo101@gmail.com>
2025-03-12 13:29:03 +0530

    Destructure and handle the release connection request path

---
+++ src/main/java/lk/ijse/dep13/wenasconnectionpool/ReleaseConnectionServlet.java
@@ -0,0 +1,45 @@
+package lk.ijse.dep13.wenasconnectionpool;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@WebServlet(name = "release-connection-servlet", urlPatterns = "/connections/*")
+public class ReleaseConnectionServlet extends HttpServlet {
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
+            releaseAllConnections(resp);
+            return;
+        }
+
+        Matcher matcher = Pattern.compile("^/(?<id>[^/])/?(?<rest>.*)$").matcher(req.getPathInfo());
+        if (matcher.find()) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        String rest = matcher.group("rest");
+        if (!rest.isBlank()) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        String id = matcher.group("id");
+        releaseConnection(Integer.valueOf(id.trim()), resp);
+    }
+
+    private void releaseConnection(Integer id, HttpServletResponse res) {
+        System.out.println("ReleaseConnectionServlet");
+    }
+
+    private void releaseAllConnections(HttpServletResponse resp) {
+        System.out.println("ReleaseAllConnectionServlet");
+    }
+}
