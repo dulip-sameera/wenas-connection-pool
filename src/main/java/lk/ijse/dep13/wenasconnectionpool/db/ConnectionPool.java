53c86c40b134c12ca41a713aad7b4082d4c60c14
Dulip Sameera <dsgitrepo101@gmail.com>
2025-03-12 13:53:38 +0530

    Add connection handle methods

--- src/main/java/lk/ijse/dep13/wenasconnectionpool/db/ConnectionPool.java
+++ src/main/java/lk/ijse/dep13/wenasconnectionpool/db/ConnectionPool.java
@@ -2,6 +2,8 @@ package lk.ijse.dep13.wenasconnectionpool.db;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -29,6 +31,58 @@ public class ConnectionPool {
 
     public ConnectionPool(int poolSize) {
         this.poolSize = poolSize;
+        try {
+            initializer();
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void initializer() throws SQLException, ClassNotFoundException {
+        String database = PROPERTIES.getProperty("app.db.database");
+        String host = PROPERTIES.getProperty("app.db.host");
+        String port = PROPERTIES.getProperty("app.db.port");
+        String user = PROPERTIES.getProperty("app.db.user");
+        String password = PROPERTIES.getProperty("app.db.password");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        for (int i = 0; i < this.poolSize; i++) {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://%s:%s/$s".formatted(host, port, database), user, password);
+            MAIN_POOL.put(UUID.randomUUID(), connection);
+        }
+    }
+
+    public record ConnectionWrapper(UUID id, Connection connection) {
+    }
+
+    public synchronized ConnectionWrapper getConnection() {
+        while (MAIN_POOL.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        UUID key = MAIN_POOL.keySet().stream().findFirst().get();
+        Connection connection = MAIN_POOL.get(key);
+        MAIN_POOL.remove(key);
+        CONSUMER_POOL.put(key, connection);
+        return new ConnectionWrapper(key, connection);
+    }
+
+    public synchronized void releaseConnection(UUID id) {
+        if (!CONSUMER_POOL.containsKey(id)) throw new RuntimeException("Invalid Connection ID");
+        Connection connection = CONSUMER_POOL.get(id);
+        CONSUMER_POOL.remove(id);
+        MAIN_POOL.put(id, connection);
+        notify();
+    }
+
+    public synchronized void releaseAllConnections() {
+        CONSUMER_POOL.forEach(MAIN_POOL::put);
+        CONSUMER_POOL.clear();
+        notifyAll();
     }
 
 }
